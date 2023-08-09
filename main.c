cde3ac0c0c1230b18b815024a1acd59e3f31da57
fischer <108022572+fischerxp@users.noreply.github.com>
2023-08-08 23:53:27 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Escreva os números múltiplos de 3 no intervalo de 153 a 300.*/
+/*desenvolvedor: fischerxp*/
+
+int main()
+{
+    int num;
+    for(num = 153; num <= 300; num+=3)
+    printf("%d\n", num);
+    return 0;
+}
